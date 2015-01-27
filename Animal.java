ea5bd9a4db92d9baf2987030034746858b56c2e2
janos94 <osinga.jan@gmail.com>
2015-01-27 08:53:16 +0100

    Commentaar Project Groep 3

--- Animal.java
+++ Animal.java
@@ -3,7 +3,7 @@ import java.util.List;
 /**
  * A class representing shared characteristics of animals.
  * 
- * @author David J. Barnes and Michael Kölling
+ * @author Project groep 3
  * @version 2011.07.31
  */
 public abstract class Animal
